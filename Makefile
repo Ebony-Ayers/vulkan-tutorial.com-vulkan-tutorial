0e09c420f7f333530ff063b3a2a232197ca7fa2b
Joseph Davies <daviesjd1@gmail.com>
2020-03-21 17:39:36 +1100

    Chapter: Introduction

--- Makefile
+++ Makefile
@@ -1,15 +1,19 @@
 platform ?= -DPLATFORM=LINUX
 platform_flags = $(platform)
-debug_flags ?= -DDEBUG -D_GLIBCXX_DEBUG -DTRACK_MEM_ALLOC -DPRINT_MEM_ALLOC
+debug_flags ?= -DDEBUG -D_GLIBCXX_DEBUG #-DTRACK_MEM_ALLOC -DPRINT_MEM_ALLOC
+
+VULKAN_SDK_PATH = /vulkan/1.2.131.2/x86_64
+vulkan_compile_flags = -I$(VULKAN_SDK_PATH)/include
+vulkan_linker_flags = -L$(VULKAN_SDK_PATH)/lib `pkg-config --static --libs glfw3` -lvulkan
 
 pch = pch.h.gch
 object_files = main.o
 
 output: $(object_files) $(pch) Makefile
-	g++ -std=c++17 $(object_files) -o output
+	g++ -std=c++17 $(object_files) -o output $(vulkan_linker_flags)
 
 main.o: main.cpp $(pch) Makefile
-	g++ -std=c++17 -c main.cpp -o main.o $(platform_flags) $(debug_flags)
+	g++ -std=c++17 -c main.cpp -o main.o $(platform_flags) $(debug_flags) $(vulkan_compile_flags)
 
 
 
--- main.cpp
+++ main.cpp
@@ -5,9 +5,18 @@
 #include "pch.h"
 
 //external dependancies
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm/vec4.hpp>
+#include <glm/mat4x4.hpp>
 
 //indernal dependancies
 
+
+//memory tracking
 #if (TRACK_MEM_ALLOC)
 static uint32_t total_memory_allocated = 0;
 static uint32_t number_of_allocations = 0;
@@ -16,7 +25,7 @@ static uint32_t number_of_frees = 0;
 void* operator new(size_t size)
 {
 	#if (PRINT_MEM_ALLOC)
-	std::cout << number_of_allocations << "Allocating " << size << "bytes, total memory allocated = " << total_memory_allocated + size << " bytes\n";
+	std::cout << number_of_allocations << " Allocating " << size << "bytes, total memory allocated = " << total_memory_allocated + size << " bytes\n";
 	#endif
 	total_memory_allocated += size;
 	number_of_allocations++;
@@ -26,7 +35,7 @@ void* operator new(size_t size)
 void operator delete(void* memory, size_t size)
 {
 	#if (PRINT_MEM_ALLOC)
-	std::cout << number_of_frees << "Freeing " << size << "bytes, new total memory allocated = " << total_memory_allocated - size << " bytes\n";
+	std::cout << number_of_frees << " Freeing " << size << "bytes, new total memory allocated = " << total_memory_allocated - size << " bytes\n";
 	#endif
 	total_memory_allocated -= size;
 	number_of_frees++;
@@ -40,7 +49,28 @@ int main(int argc, char* argv[])
 	std::cout << "Running in debug mode\ngcc version: " << __GNUC__ << "." << __GNUC_MINOR__ << "." << __GNUC_PATCHLEVEL__ << "\n";
 	#endif
 
+	glfwInit();
 
+	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+	GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
+
+	uint32_t extentionCount = 0;
+	vkEnumerateInstanceExtensionProperties(nullptr, &extentionCount, nullptr);
+
+	std::cout << extentionCount << " 6extensions supported" << std::endl;
+
+	glm::mat4 matrix;
+	glm::vec4 vector;
+	auto test = matrix * vector;
+
+	while(!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents();
+	}
+
+	glfwDestroyWindow(window);
+
+	glfwTerminate();
 
 	#if (DEBUG)
 	std::cout << "Exiting application\n";
