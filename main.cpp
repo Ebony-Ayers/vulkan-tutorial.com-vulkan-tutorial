132c0f9d375d580c9ef4a6d7bec3cbb9d1e81631
Joseph Davies <daviesjd1@gmail.com>
2020-03-22 21:38:42 +1100

    Chapter: Logical devices and queues

--- main.cpp
+++ main.cpp
@@ -87,6 +87,8 @@ class HelloTringleApplication
 		VkInstance instance;
 		VkDebugUtilsMessengerEXT debugMessenger;
 		VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;
+		VkDevice device;
+		VkQueue graphicsQueue;
 
 		void initWindow()
 		{
@@ -103,6 +105,7 @@ class HelloTringleApplication
 			createInstance();
 			setupDebugMessenger();
 			pickPysicalDevice();
+			createLogicalDevice();
 		}
 
 		void mainLoop()
@@ -115,6 +118,8 @@ class HelloTringleApplication
 
 		void cleanup()
 		{
+			vkDestroyDevice(device, nullptr);
+			
 			if(enableValidationLayers)
 			{
 				DestroyDebugUtilsMessengerEXT(instance, debugMessenger, nullptr);
@@ -187,6 +192,7 @@ class HelloTringleApplication
 					if(std::strcmp(layerProperties.layerName, validationLayers[i]) == 0)
 					{
 						in_use = true;
+						break;
 					}
 				}
 				if(in_use)
@@ -427,6 +433,46 @@ class HelloTringleApplication
 			}
 
 			return indices;
+		}
+
+		void createLogicalDevice()
+		{
+			QueueFamilyIndicies indicies = findQueueFamilies(physicalDevice);
+
+			VkDeviceQueueCreateInfo queueCreateInfo = {};
+			queueCreateInfo.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
+			queueCreateInfo.queueFamilyIndex = indicies.graphicsFamily.value();
+			queueCreateInfo.queueCount = 1;
+			float queuePriority = 1.0f;
+			queueCreateInfo.pQueuePriorities = &queuePriority;
+
+			VkPhysicalDeviceFeatures deviceFeatures = {};
+
+			VkDeviceCreateInfo createInfo = {};
+			createInfo.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
+			createInfo.pQueueCreateInfos = &queueCreateInfo;
+			createInfo.queueCreateInfoCount = 1;
+
+			createInfo.pEnabledFeatures = &deviceFeatures;
+
+			createInfo.enabledExtensionCount = 0;
+
+			if(enableValidationLayers)
+			{
+				createInfo.enabledLayerCount = static_cast<uint32_t>(validationLayers.size());
+				createInfo.ppEnabledLayerNames = validationLayers.data();
+			}
+			else
+			{
+				createInfo.enabledLayerCount = 0;
+			}
+
+			if(vkCreateDevice(physicalDevice, &createInfo, nullptr, &device) != VK_SUCCESS)
+			{
+				throw std::runtime_error("failed to create logical device!");
+			}
+
+			vkGetDeviceQueue(device, indicies.graphicsFamily.value(), 0, &graphicsQueue);
 		}
 };
 
