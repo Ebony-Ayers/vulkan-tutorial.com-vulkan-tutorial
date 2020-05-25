67412f3d2a17743da57ce38d1a7082f415e28ce1
Joseph Davies <daviesjd1@gmail.com>
2020-05-25 19:08:33 +1000

    Chapter: Image view and sampler

--- shaders/complile.sh
+++ shaders/complile.sh
@@ -1,2 +1,3 @@
-/vulkan/1.2.131.2/x86_64/bin/glslc shader.vert -o vert.spv
-/vulkan/1.2.131.2/x86_64/bin/glslc shader.frag -o frag.spv
+/code-libraries/cpp/vulkan/latest/x86_64/bin/glslc shader.vert -o vert.spv
+/code-libraries/cpp/vulkan/latest/x86_64/bin/glslc shader.frag -o frag.spv
+
